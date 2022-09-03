0a3e4b2056d1fbb5669d365ee582737609a9a1d2
ahdee <simplitia@gmail.com>
2022-09-03 10:30:40 -0700

    get sequence range and start getting the final break pts

--- CAFIN/prepare_ccds.R
+++ CAFIN/prepare_ccds.R
@@ -1,6 +1,7 @@
 library ( dplyr )
 library ( glue )
 library( stringr )
+library("Biostrings")
 bp= 20 # what length you want 
 
 samtools = '/ehome/bin/samtools_rstudio/samtools-1.15.1/bin/bin/samtools'
@@ -34,7 +35,8 @@ getseq<- function(range, strand, build_pos, build_neg){
 }
 
 
-# takes a ccds formated file and return the coordinates for 5' and 3' partners 
+# takes a ccds formated file and return the coordinates if exons is a 5' and 3' partners
+# two things need to be consider, strand and whether the gene is on the 5' or  3' fusion. 
 # bp is how many extra base pair to get 
 # this will only return coordinates and the actual seq for that use getseq
 
@@ -102,6 +104,120 @@ get_db = function ( file , bp ){
   
 }
 
+
+
+# slurp fastq  into dataframe
+fastqToDF <- function ( file, cname=NA ){
+
+  
+  
+  fastaFile <- readDNAStringSet(  file  )
+  seq_name = names(fastaFile)
+  sequence = paste(fastaFile)
+  df <- data.frame(match = seq_name, cname=sequence)
+  
+  colnames ( df ) = c( 'match', cname)
+  
+  return ( df )
+  
+}
+
+getSeqRange <- function ( use_index, use_genome ) {
+  # get sequences 
+  # depending on the strand and whether its for 5' or 3' the commands vary. 
+  # thus will split into 4 different tables 
+  
+  # for testing only 
+  ## use_index = ihg8
+  ## use_genome = hg38 
+  
+  # split into + and - strands since sequences for negative requires
+  # complimentary reverse 
+  
+  pos = use_genome[use_genome$cds_strand=="+", ]
+  neg = use_genome[use_genome$cds_strand=="-", ]
+  
+  ## there are going to be total of 2 columns ( 5' and 3' )
+  ### however we need to run this twice, one for + and then - 
+  ### samtools used to pull out sequence of each region 
+  
+  ### 5' positive strand 
+  
+  
+  posin5 = paste0(out_dir, 'fusion5p_pos', ".IN.tmp")
+  posout5 =  paste0(out_dir, 'fusion5p_pos', ".OUT.tmp")
+  write( pos$fusion_5_prime , posin5 , append = F)
+  cmd = glue ( samtools, " faidx ", use_index, ' -r ', posin5 , " -o ",  posout5 )
+  system ( cmd , intern = T )
+  
+  
+  
+  ### 5' negative strand
+  
+  negin5 = paste0(out_dir, 'fusion5p_neg', ".IN.tmp")
+  negout5 =  paste0(out_dir, 'fusion5p_neg', ".OUT.tmp")
+  write( neg$fusion_5_prime , negin5 , append = F)
+  cmd = glue ( samtools, " faidx ", use_index, ' --reverse-complement -r ', negin5 , " -o ",  negout5 )
+  system ( cmd , intern = T )
+  
+  
+  ### 3' positive strand
+  posin3 = paste0(out_dir, 'fusion3p_pos', ".IN.tmp")
+  posout3 =  paste0(out_dir, 'fusion3p_pos', ".OUT.tmp")
+  write( pos$fusion_3_prime , posin3 , append = F)
+  cmd = glue ( samtools, " faidx ", use_index, ' -r ', posin3 , " -o ",  posout3 )
+  system ( cmd , intern = T )
+  
+  
+  ### 3' negative strand
+  negin3 = paste0(out_dir, 'fusion3p_neg', ".IN.tmp")
+  negout3 =  paste0(out_dir, 'fusion3p_neg', ".OUT.tmp")
+  write( neg$fusion_3_prime , negin3 , append = F)
+  cmd = glue ( samtools, " faidx ", use_index, ' --reverse-complement -r ', negin3 , " -o ",  negout3 )
+  system ( cmd , intern = T )
+  
+  
+  # conver fastq to df and merge 
+  
+  pos5 = fastqToDF ( posout5 , cname = "5'_seq")
+  pos3 = fastqToDF ( posout3 , cname = "3'_seq")
+  
+  # don't merge 
+  pos = cbind ( pos, pos5 )
+  p1 = all.equal(pos$fusion_5_prime, pos$match) # this need to be true to preserve order
+  pos$match = NULL 
+  
+  pos = cbind ( pos, pos3 )
+  p2 = all.equal(pos$fusion_3_prime, pos$match)
+  pos$match = NULL 
+  
+  ## negative strand 
+  neg5 = fastqToDF ( negout5 , cname = "5'_seq")
+  neg3 = fastqToDF ( negout3 , cname = "3'_seq")
+  # remove /rc trailing
+  neg5$match = gsub ( "\\/rc", "", neg5$match )
+  neg3$match = gsub ( "\\/rc", "", neg3$match )
+  
+  neg = cbind ( neg, neg5 )
+  p3 = all.equal(neg$fusion_5_prime, neg$match) # this need to be true to preserve order
+  neg$match = NULL 
+  
+  neg = cbind ( neg, neg3 )
+  p4 = all.equal(neg$fusion_3_prime, neg$match)
+  neg$match = NULL 
+  
+  if (  all ( p1,p2,p3,p4 ) ) {
+    final_df = rbind ( pos, neg )
+  } else {
+    return ( "ERROR: something went wrong and unable to find sequences")
+  }
+  
+  return ( final_df )
+  
+  
+}
+
+
 file =  "./CCDS/hg38/CCDS.GRCh38.p12.txt"
 
 hg38 = get_db ( file, bp=25)
@@ -117,86 +233,47 @@ hg38[hg38$gene=="FLI1", ]
 hg38[hg38$gene=="PRIM1", ] 
 hg38[hg38$gene=="NACA", ] 
 
+### 
+hg38s = getSeqRange( ihg8, hg38)
+
+hg38s[hg38s$gene=="EWSR1", ] 
+hg38s[hg38s$gene=="FLI1", ] 
+
+hg38s[hg38s$gene=="PRIM1", ] 
+hg38s[hg38s$gene=="NACA", ] 
 
 
-# get sequences 
-# depending on the strand and whether its for 5' or 3' the commands vary. 
-# thus will split into 4 different tables 
+combodf = hg38s 
 
-pos = hg38[hg38$cds_strand=="+", ]
-neg = hg38[hg38$cds_strand=="-", ]
+can = c("EWSR1-FLI1", "PRIM1-NACA")
 
-
-# get positive strand 5' 
-
-
-posin5 = paste0(out_dir, 'fusion5p_pos', ".IN.tmp")
-posout5 =  paste0(out_dir, 'fusion5p_pos', ".OUT.tmp")
-write( pos$fusion_5_prime , posin5 , append = F)
-# ihg8 is the index 
-cmd = glue ( samtools, " faidx ", ihg8, ' -r ', posin5 , " -o ",  posout5 )
-system ( cmd , intern = T )
-
-# get negative strand 5' 
-
-negin5 = paste0(out_dir, 'fusion5p_neg', ".IN.tmp")
-negout5 =  paste0(out_dir, 'fusion5p_neg', ".OUT.tmp")
-write( neg$fusion_5_prime , negin5 , append = F)
-# ihg8 is the index 
-cmd = glue ( samtools, " faidx ", ihg8, ' --reverse-complement -r ', negin5 , " -o ",  negout5 )
-system ( cmd , intern = T )
-
-### 3' positive
-posin3 = paste0(out_dir, 'fusion3p_pos', ".IN.tmp")
-posout3 =  paste0(out_dir, 'fusion3p_pos', ".OUT.tmp")
-
-write( pos$fusion_3_prime , posin3 , append = F)
-
-
-cmd = glue ( samtools, " faidx ", ihg8, ' -r ', posin3 , " -o ",  posout3 )
-system ( cmd , intern = T )
-
-
-### 3' neg
-negin3 = paste0(out_dir, 'fusion3p_neg', ".IN.tmp")
-negout3 =  paste0(out_dir, 'fusion3p_neg', ".OUT.tmp")
-
-write( neg$fusion_3_prime , negin3 , append = F)
-
-
-cmd = glue ( samtools, " faidx ", ihg8, ' --reverse-complement -r ', negin3 , " -o ",  negout3 )
-system ( cmd , intern = T )
-
-
-
-
-
-library("Biostrings")
-
-fastaFile <- readDNAStringSet(  paste0(out_dir, 'fusion5p_pos', ".OUT_tmp") )
-seq_name = names(fastaFile)
-sequence = paste(fastaFile)
-df <- data.frame(seq_name, sequence)
-
-
-
-
-
-
-hg38$`3'_seq` = as.character ( mapply(getseq, 
-                                         range=hg38$fusion_3_prime, 
-                                         strand=hg38$cds_strand, 
-                                         build_pos =chg8_pos, 
-                                         build_neg=chg8_neg )
-)
-
-
-
-
-
-
-
-
+for ( fc in can ){
+  
+  temp = unlist ( stringr::str_split( fc, "-") )
+  gene5 = temp[1]
+  gene3 = temp[2]
+  
+  j5 = combodf[combodf$gene == gene5, ]$`5'_seq`
+  j3 = combodf[combodf$gene == gene3, ]$`3'_seq`
+  
+  testj = c()
+  
+  # By adding the --ignore-case option the command becomes 57x times slower 
+  # so will not lowercase 
+  
+  for ( j5s in j5 ){
+    for ( j3s in j3){
+      seq = paste0(j5s, "", j3s )
+      testj= c( testj, seq)
+    }
+  }
+  
+  # for EWSR1-FLI set to: "GCTACGGGCAGCAGAacccttcttatgactca"
+  testj[grepl ( 'GCTACGGGCAGCAGAacccttcttatgactca' , testj, ignore.case = T) ]
+  
+  
+  
+}
 
 
 
